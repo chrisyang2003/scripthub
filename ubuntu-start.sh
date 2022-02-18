73cfb2421fd32d973aee821c65533dd6cbabe5a4
chrisyang2003 <1017975501@qq.com>
2022-02-18 10:50:52 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -1,2 +1,13 @@
 curl net-tools unar vim nodejs npm openssh-server
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+
+
+#nodejs
+# wget https://nodejs.org/dist/v14.16.1/node-v14.16.1-linux-x64.tar.xz
+# tar xf node-v14.16.1-linux-x64.tar.xz 
+# sudo ln -s /home/ubuntu/bin/node-v14.16.1-linux-x64/bin/node /usr/bin/node
+# sudo ln -s /home/ubuntu/bin/node-v14.16.1-linux-x64/bin/npm /usr/bin/npm
+# npm config set registry https://registry.npm.taobao.org
+
+
+
