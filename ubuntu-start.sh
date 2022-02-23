d01dc308169ce02b47cb49b8b6aef08d43cbf638
chrisyang2003 <1017975501@qq.com>
2022-02-23 13:36:22 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -4,21 +4,29 @@
 # library
 sudo apt-get install libssl-dev build-essential automake pkg-config libtool libffi-dev libgmp-dev
 
-curl net-tools unar vim nodejs npm openssh-server
+# tool
+sudo apt-get install curl net-tools unar vim nodejs npm openssh-server
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 
 
 #nodejs
+cd ~
+mkdir ./bin
+cd ./bin
 wget https://nodejs.org/dist/v14.16.1/node-v14.16.1-linux-x64.tar.xz
 tar xvf node-v14.16.1-linux-x64.tar.xz 
-sudo ln -s `pwd`/node-v14.16.1-linux-x64/bin/node /usr/bin/node
-sudo ln -s `pwd`/node-v14.16.1-linux-x64/bin/npm /usr/bin/npm
+sudo ln -s ~/bin/node-v14.16.1-linux-x64/bin/node /usr/bin/node
+sudo ln -s ~/bin/node-v14.16.1-linux-x64/bin/npm /usr/bin/npm
 npm config set registry https://registry.npm.taobao.org
 
 #golang
+cd ~
+mkdir ./bin
+cd ./bin
 wget https://go.dev/dl/go1.17.7.linux-amd64.tar.gz
 tar zxvf go1.17.7.linux-amd64.tar.gz
-# export PATH=$PATH:/root/chrisyy/go/bin
+echo "export PATH=\$PATH:~/bin/go/bin" >> ~/.bashrc
+source .bashrc
 
 #zsh
 sudo apt install zsh
